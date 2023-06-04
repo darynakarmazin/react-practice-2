a233e1b616812b9fad0cc7cc4dcfdc251a13a398
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-06-04 16:24:25 +0300

    Added todos

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,33 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  };
+
+  handleSubmit = text => {
+    const todo = { text, id: nanoid() };
+    this.setState(prevState => ({
+      todos: [...prevState.todos, todo],
+    }));
+    console.log(text);
+  };
+
   render() {
-    return <Text>Todos</Text>;
+    const { todos } = this.state;
+    return (
+      <>
+        <SearchForm onSubmit={this.handleSubmit} />
+        <Grid>
+          {todos.map(({ text, id }, index) => {
+            return (
+              <GridItem key={id}>
+                <Todo text={text} counter={index+1} />
+              </GridItem>
+            );
+          })}
+        </Grid>
+      </>
+    );
   }
 }
