8a7aad6009d1bcd76f6e02eddc85366fd4c2f34e
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-06-04 14:58:43 +0300

    Added gallery fn

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,34 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    query: '',
+  };
+
+  handleChange = event => {
+    this.setState({ query: event.target.value });
+  };
+
+  formSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.query);
+    this.setState({ query: '' });
+  };
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.formSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          value={this.state.query}
+          onChange={this.handleChange}
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,9 +4,41 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    query: '',
+    page: 1,
+    images: [],
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+
+    if (prevState.query !== query) {
+      this.getImages(query, page);
+    }
+  }
+
+  getImages = async (query, page) => {
+    const result = await ImageService.getImages(query, page);
+    this.setState(prevState => ({
+      images: [...prevState.images, result.photos],
+    }));
+    console.log(result);
+  };
+
+  // componentDidMount() {
+  //   ImageService.getImages('dog', 1);
+  // }
+
+  handleSumit = query => {
+    this.setState({ query });
+    // console.log(query);
+  };
+
   render() {
     return (
       <>
+        <SearchForm onSubmit={this.handleSumit} />
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
